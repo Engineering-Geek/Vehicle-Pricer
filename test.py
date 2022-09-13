a9b1da8b862a2722806fdf65f42f5f7aa3e0a869
Nikhil Melgiri <nmelgiri@uwaterloo.ca>
2022-09-12 21:32:36 -0400

    test

--- test.py
+++ test.py
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import boto3
-from datetime import datetime, timezone
-
-today = datetime.now(timezone.utc)
-
-s3 = boto3.client('s3')
-
-objects = s3.list_objects(Bucket='sagemaker-vehicle-pricer-data')
-
-print(objects["Contents"][0])
-=======
 from datetime import datetime
 
 import boto3
