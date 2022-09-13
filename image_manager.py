666ebd2391aafeef5663d0fdd8a5222f4d89562d
Nikhil Melgiri <melgirinik@gmail.com>
2022-09-13 00:52:54 +0000

    image manager update

--- image_manager.py
+++ image_manager.py
@@ -49,11 +49,7 @@ class ImageManager:
             for i, url in enumerate(tqdm(choices(urls, k=int(len(urls)*fraction)), 
                                          desc='downloading images for {} {} {}'.format(row['Make'], row['Model'], row['Year']), 
                                          total=int(len(urls)*fraction))):
-<<<<<<< HEAD
-                filepath = 'images/{}/{}/{}/{}/_{}.jpg'.format(row['Make'], row['Model'], row['Year'], row["MSRP"], i)
-=======
                 filepath = 'images/{}/{}/{}/{}.jpg'.format(row['Make'], row['Model'], row['Year'], i)
->>>>>>> 75750cc2b63934bc76f0d85247ce280f8db7351b
                 image_filepaths.append(filepath)
                 self._url_image_to_bucket(template + url, filepath)
                 
